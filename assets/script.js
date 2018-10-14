dba34607d48447f2bc19b8417e6b2a6884ff043c
Spinozist <ewoodworth@atlantaregional.org>
2018-10-14 16:57:40 -0400

    basic layout with Materialize

---
+++ assets/script.js
@@ -0,0 +1,57 @@
+$(document).ready(function () {
+    // $('.parallax').parallax();
+    $('.collapsible').collapsible();
+
+
+// $(`#game-tab`).on(`click`, function () {
+//     var gameCarousel = $(`<div>`);
+//     gameCarousel.attr(`class`,`carousel`);
+//     gameCarousel.html(`
+//     <a class="carousel-item" href="#one!">
+//         <div class="card m2 #cfd8dc blue-grey lighten-4">
+//             <div class="card-content">
+//                 <span class="card-title">Front End:</span>
+//                 <div>
+//                     <img src="assets/images/front-end-logos.png" alt="front-end-logos">
+//                 </div>
+//             </div>
+//         </div>
+//     </a>
+//     <a class="carousel-item" href="#one!">
+//         <div class="card m2 #cfd8dc blue-grey lighten-4">
+//             <div class="card-content">
+//                 <span class="card-title">Front End:</span>
+//                 <div>
+//                     <img src="assets/images/front-end-logos.png" alt="front-end-logos">
+//                 </div>
+//             </div>
+//         </div>
+//     </a> <a class="carousel-item" href="#one!">
+//         <div class="card m2 #cfd8dc blue-grey lighten-4">
+//             <div class="card-content">
+//                 <span class="card-title">Front End:</span>
+//                 <div>
+//                     <img src="assets/images/front-end-logos.png" alt="front-end-logos">
+//                 </div>
+//             </div>
+//         </div>
+//     </a> <a class="carousel-item" href="#one!">
+//         <div class="card m2 #cfd8dc blue-grey lighten-4">
+//             <div class="card-content">
+//                 <span class="card-title">Front End:</span>
+//                 <div>
+//                     <img src="assets/images/front-end-logos.png" alt="front-end-logos">
+//                 </div>
+//             </div>
+//         </div>
+//     </a>
+//     `);
+//    gameCarousel.carousel();
+//    $(`#game-box`).html(gameCarousel);
+   
+// });
+});
+
+
+
+
